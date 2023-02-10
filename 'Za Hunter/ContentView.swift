5b8e7274668957cf1f8845192e36f73ef2af9701
Erin Chon <echon@bsd220.org>
2023-02-10 09:39:16 -0600

    Initial Commit

---
+++ 'Za Hunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  'Za Hunter
+//
+//  Created by Erin Chon on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Erin Chon on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
